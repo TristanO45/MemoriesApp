e0010eeed7d13fd458859ff3327a0dde486350ca
Your Name <tonfroy90@gmail.com>
2023-06-14 21:35:19 -0700

    Working on like count

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -22,7 +22,7 @@ const Posts = ({ setCurrentId }) => {
     >
       {posts.map((post) => (
         <Grid key={post._id} item xs={12} sm={6}>
-          <Post post={post} setCurrentId={setCurrentId}/>
+          <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
     </Grid>
